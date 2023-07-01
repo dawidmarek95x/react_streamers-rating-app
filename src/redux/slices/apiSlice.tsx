d618dcddcbaaadb1cf1ba9dc2282e0048e67e05c
Dawid Marek <dawid.marek95x@gmail.com>
2023-07-01 22:28:51 +0200

    Added support for streamers endpoint with API

--- src/redux/slices/apiSlice.tsx
+++ src/redux/slices/apiSlice.tsx
@@ -1,15 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const apiKey = "http://localhost:3001/";
-
-const api =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development" || !apiKey
-    ? "http://localhost:3001/"
-    : `${apiKey}`;
-
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `${api}`,
+    baseUrl: "/",
     prepareHeaders(headers) {
       return headers;
     },
